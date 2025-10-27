049ec3b0700b8a52afbd35cee13888c0c01e8923
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2025-10-26 23:34:05 -0300

    Configure sidekiq as job queue adapter

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module SimpleInvestmentPlatform
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Set the active job queue adapter to sidekiq
+    config.active_job.queue_adapter = :sidekiq
   end
 end
