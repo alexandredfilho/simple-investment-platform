8851ef4fee28dbbd42775ac699751a51d5bddc60
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2025-10-26 23:27:36 -0300

    Add ransackable attributes and associations, plus total invested methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
 # app/models/user.rb
 class User < ApplicationRecord
+  has_many :investments, inverse_of: :user, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 255 }
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ 'created_at', 'email', 'id', 'name', 'updated_at' ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ 'investments' ]
+  end
+
+  def total_invested_cents
+    investments.sum(:amount_cents)
+  end
+
+  def total_invested
+    total_invested_cents.to_d / 100
+  end
 end
