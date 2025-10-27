a25c93cd1b72bbe614c161f337e6ebb51b752de8
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2025-10-26 23:35:39 -0300

    Add new routes for sidekiq web and also investments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   root "dashboards#index"
 
   resources :users
   resources :fundraises
+  resources :investments
 end
